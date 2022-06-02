ab5049d043bffd1b4f800b514f13a8fb51ca40d4
Tristan Whang <twhang@usc.edu>
2022-06-01 21:24:20 -0700

    initial commit

---
+++ totallyNotSuperMario/empty-example/Player.js
@@ -0,0 +1,21 @@
+
+// this all seems to work when the player code and class definition is in
+// sketch.js but not when its in another file.
+
+// class Player {
+//     constructor() {
+//         this.hitBoxWidth = 100;
+//         this.hitBoxHeight = 100;
+//         this.x = 100;
+//         this.y = 400;
+//     }
+//     show() {
+//         console.log("showing");
+//         // push();
+//         image(playerImage, this.x, this.y, this.hitBoxHeight, this.hitBoxWidth);
+//         // pop();
+//     }
+//     move() {
+//         this.x += 1;
+//     }
+// }
---
+++ totallyNotSuperMario/empty-example/sketch.js
@@ -0,0 +1,56 @@
+// let playerImage = null;
+let player = null;
+let playerImage = null;
+
+function preload() {
+    console.log("Preloading");
+    playerImage = loadImage('idle.png');
+}
+
+function setup() {
+    // put setup code here
+    createCanvas(1200, 950);
+    player = new Player();
+}
+let x = 0;
+function draw() {
+    // console.log("Drawing");
+    // put drawing code here
+    background(220);
+    // translate(0,50);
+    player.show();
+    player.move();
+    ellipse(mouseX, height/2, 20, 20);
+
+    // player.Update();
+    // image(playerImage, x, 400, 100, 100);
+    
+
+    // push();
+
+    // if (mouseIsPressed) {
+    //   fill(0);
+    // } else {
+    //   fill(255);
+    // }
+    // ellipse(mouseX, mouseY, 80, 80);
+    // pop();
+    
+}
+class Player {
+    constructor() {
+        this.hitBoxWidth = 100;
+        this.hitBoxHeight = 100;
+        this.x = 100;
+        this.y = 400;
+    }
+    show() {
+        console.log("showing");
+        // push();
+        image(playerImage, this.x, this.y, this.hitBoxHeight, this.hitBoxWidth);
+        // pop();
+    }
+    move() {
+        this.x += 1;
+    }
+}
